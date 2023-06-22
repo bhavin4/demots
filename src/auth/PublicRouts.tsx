4ede9de8e547b7e1d1e69ecbe9f22922228e7f81
Vrushant <vrushantkathiriya@gmail.com>
2023-06-22 12:24:58 +0530

    changes

--- src/auth/PublicRouts.tsx
+++ src/auth/PublicRouts.tsx
@@ -3,11 +3,7 @@ import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 
 type Props = {
-<<<<<<< HEAD
-  children: ReactNode;
-=======
   children: ReactNode;  
->>>>>>> 5535a0e3dd89b0505845664aea17c1f72bad9667
 };
 
 const PublicRoutes = ({ children }: Props) => {
@@ -18,11 +14,7 @@ const PublicRoutes = ({ children }: Props) => {
       {!cookies.token ? (
         <>{children}</>
       ) : (
-<<<<<<< HEAD
-        <Navigate to="/LoginForm" replace={true} />
-=======
         <Navigate to="/RegestrationForm" replace={true} />
->>>>>>> 5535a0e3dd89b0505845664aea17c1f72bad9667
       )}
     </>
   );
--- src/utils/Routes.ts
+++ src/utils/Routes.ts
@@ -1,16 +1,16 @@
-  import LoginForm from "../Pages/LoginForm"
   import RegestrationForm from "../Pages/RegestrationForm"
   import PublicRoutes from "../auth/PublicRouts"
-  import LoginPage from "../components/LoginPage";
+  import LoginPage from "../Components/Containers/LoginForm";
+import LoginForm from "../Components/Containers/LoginForm";
   export const AppRoutes = [
   
-    // {
-    //   id: "0",
-    //   path: "/",
-    //   name: "loginForm",
-    //   component: LoginForm,
-    //   auth: PublicRoutes,
-    // },
+    {
+      id: "0",
+      path: "/",
+      name: "loginForm",
+      component: LoginForm,
+      auth: PublicRoutes,
+    },
 
     {
       id: "0",
