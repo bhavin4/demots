44557a5cdb12ae80c0d39c4450647e533a191126
Bhavin Sukhadiya <125634765+bhavin4@users.noreply.github.com>
2023-06-22 17:20:16 +0530

    add validation and api

--- src/Pages/RegistrationPage.tsx
+++ src/Pages/RegistrationPage.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
-import LoginForm from "../components/LoginForm";
+import RegestrationForm from "../components/RegestrationForm";
 
 type Props = {};
 
-const LoginPage = (props: Props) => {
+const RegistrationPage = (props: Props) => {
   const [first, setfirst] = useState("");
 
   return (
     <div className="box-border h-screen container  bg-secondary">
     <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-screen  justify-between">
       <div className="col-span-1">
-        <LoginForm />
+        <RegestrationForm />
       </div>
       <div className="col-span-1 hidden lg:flex w-full h-screen">
         <img
@@ -24,4 +24,4 @@ const LoginPage = (props: Props) => {
   );
 };
 
-export default LoginPage;
+export default RegistrationPage;
